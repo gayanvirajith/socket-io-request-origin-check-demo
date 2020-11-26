92edee276389cda5c1f2bf85f2ace1d58c69f365
Gayan Virajith <gayanvirajith@gmail.com>
2020-11-27 01:36:06 +0530

    readables

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const { v4: uuidv4 } = require('uuid');
 const server = http.createServer();
 const io = require("socket.io")(server, {
   cors: {
+    // cors policy supports for below domains
     origin: ['http://localhost:5000', 'http://localhost:8080'], //todo get from config
     methods: ["GET", "POST"]
   }
@@ -11,14 +12,19 @@ const io = require("socket.io")(server, {
 
 const PORT = process.env.PORT || 9000;
 
-io.engine.generateId = (req) => {
+/**
+ * Custom session id creation
+ */
+io.engine.generateId = () => {
     return uuidv4();
 }
 
+/**
+ * Middleware to check request header origin
+ */
 io.use((socket, next) => {
   const handshakeData = socket.request;
   const supportiveOrigin = process.env.SUPPORTED_ORIGIN || 'http://localhost:5000'
-	
   if (handshakeData.headers.origin !== supportiveOrigin) {
     console.log('frontend origin: ' + handshakeData.headers.origin);
 	  console.log('supported origin: ' + supportiveOrigin);
@@ -28,6 +34,9 @@ io.use((socket, next) => {
   return next();
 });
 
+/**
+ * Socket connection handler 
+ */
 io.on('connection', (socket) => {
   console.log(`Socket ${socket.id} connected.`);
   socket.on('disconnect', () => {
